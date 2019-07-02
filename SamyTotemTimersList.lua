0654c10d8cdf228a4ca1d6e767e256ad28f59677
samynix@gmail.com <tor.gudmestad@bouvet.no>
2019-07-02 23:30:27 +0200

    asd

--- SamyTotemTimersList.lua
+++ SamyTotemTimersList.lua
@@ -1,27 +1,16 @@
 SamyTotemTimerList = {}
 
-local function createButton(parentFrame, tX, buttonSize, name)
+local function createSecureButton(parentFrame, tX, buttonSize, name)
     local button = CreateFrame("Button", name, parentFrame, "ActionButtonTemplate, SecureActionButtonTemplate")
     button:SetWidth(buttonSize)
     button:SetHeight(buttonSize)
     button:SetPoint("BOTTOMLEFT", parentFrame, "BOTTOMLEFT", tX, 0);
     button:Show()
-
     return button
 end
 
-local function createSelectTotemButton(spell, buttonSize, name, selectTotemCallback)
-    local buttonFrame = CreateFrame("Button", name, parentFrame, "ActionButtonTemplate")
-    buttonFrame:SetWidth(buttonSize)
-    buttonFrame:SetHeight(buttonSize)
-    buttonFrame.icon:SetTexture(select(3, GetSpellInfo(spell)))
-    buttonFrame:SetScript("OnClick", selectTotemCallback)
-
-    return {buttonFrame = buttonFrame, spell = spell}
-end
-
-local function createDropTotemButton(parentFrame, tX, name)
-    local button = createButton(parentFrame, tX, SamyTotemTimersConfig:Instance().db.buttonSize, name)
+local function createDropTotemButton(instance, parentFrame, tX, name)
+    local button = createSecureButton(parentFrame, tX, SamyTotemTimersConfig:Instance().db.buttonSize, name)
     button:RegisterForDrag('LeftButton')
     button:SetScript("OnDragStart", function (self)
         self:GetParent():SetMovable(true)
@@ -45,7 +34,38 @@ local function createDropTotemButton(parentFrame, tX, name)
         end
     end)
 
+    button:SetScript("OnMouseUp", function (self, buttonClicked) 
+        if (buttonClicked == "RightButton") then 
+            instance:RefreshTotemSelectButtons(not isTotemListVisible)
+        end
+    end)
+
     return button
+end
+
+local function createSelectTotemButton(spell, buttonSize, name, selectTotemCallback)
+    local buttonFrame = CreateFrame("Button", name, parentFrame, "ActionButtonTemplate")
+    buttonFrame:SetWidth(buttonSize)
+    buttonFrame:SetHeight(buttonSize)
+    buttonFrame.icon:SetTexture(select(3, GetSpellInfo(spell)))
+    buttonFrame:SetScript("OnClick", selectTotemCallback)
+
+    return {buttonFrame = buttonFrame, spell = spell}
+end
+
+local function createTotemSelectButtons(listName, totemsInList)
+    local config = SamyTotemTimersConfig:Instance()
+    local totemSelectButtons = {}
+    for k, v in pairs(totemsInList) do
+        local selectTotemButton = createSelectTotemButton(v, config.db.buttonSize, "SamyTotemTimersFrame" .. listName .. v .. "Button", function(self)
+            selectTotem(totemButton, listName, v)
+            instance:RefreshTotemSelectButtons(false)
+        end)
+
+        table.insert(totemSelectButtons, selectTotemButton)
+    end
+
+    return totemSelectButtons
 end
 
 local function selectTotem(totemButton, listName, spell)
@@ -58,21 +78,17 @@ function SamyTotemTimerList:New(parentFrame, tX, listName, totemsInList)
     local instance  = {}
     local config = SamyTotemTimersConfig:Instance()
     local isTotemListVisible = false
-    local totemButton = createDropTotemButton(parentFrame, tX, "SamyTotemTimersFrame" .. listName .. "Button")
-    totemButton:SetScript("OnMouseUp", function (self, button) 
-        if (button == "RightButton") then 
-            instance:RefreshTotemSelectButtons(not isTotemListVisible)
+    local totemButton = createDropTotemButton(instance, parentFrame, tX, "SamyTotemTimersFrame" .. listName .. "Button")
+    local totemSelectButtons = createTotemSelectButtons(listName, totemsInList)
+
+    function instance:LoadSavedVariables()
+        if (SamyTotemTimersDB.selectedTotems[listName]) then
+            selectTotem(totemButton, listName, SamyTotemTimersDB.selectedTotems[listName])
         end
-    end)
-
-    local totemSelectButtons = {}
-    for k, v in pairs(totemsInList) do
-        local selectTotemButton = createSelectTotemButton(v, config.db.buttonSize, "SamyTotemTimersFrame" .. listName .. v .. "Button", function(self)
-            selectTotem(totemButton, listName, v)
-            instance:RefreshTotemSelectButtons(false)
-        end)
-
-        table.insert(totemSelectButtons, selectTotemButton)
+    
+        if (SamyTotemTimersDB.keyboardShortcuts[listName]) then
+            self:SetKeyboardShortcut(SamyTotemTimersDB.keyboardShortcuts[listName])
+        end
     end
 
     function instance:SetKeyboardShortcut(shortcut)
@@ -97,14 +113,6 @@ function SamyTotemTimerList:New(parentFrame, tX, listName, totemsInList)
         end
     end
 
-
-    if (SamyTotemTimersDB.selectedTotems[listName]) then
-        selectTotem(totemButton, listName, SamyTotemTimersDB.selectedTotems[listName])
-    end
-
-    if (SamyTotemTimersDB.keyboardShortcuts[listName]) then
-        instance:SetKeyboardShortcut(SamyTotemTimersDB.keyboardShortcuts[listName])
-    end
-
+    instance:LoadSavedVariables()
     return instance
 end
