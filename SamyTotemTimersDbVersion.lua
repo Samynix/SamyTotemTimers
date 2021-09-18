05b07d9a10836f5ba5b0dc2e06ec9664d5bc62c1
Tor Einar Gudmestad <samynix@gmail.com>
2021-09-18 11:44:49 +0200

    Ensured pulsetimer for tremor is 4 sec

---
+++ SamyTotemTimersDbVersion.lua
@@ -0,0 +1,30 @@
+SamyTotemTimersDbVersion = {}
+
+local function IsDoDbVersion(versionNumber, db)
+    if (db and (not db.version or db.version < versionNumber)) then
+        return true
+    end
+
+    return false
+end
+
+function SamyTotemTimersDbVersion:UpdateDatabase(samyTotemTimersDb, forceRun, setDefault)
+    if (IsDoDbVersion(2.1, samyTotemTimersDb)) then
+        isReset = true
+        SamyTotemTimersUtils:Print("Current version incompatible with old saved variables, reseting all configuration")
+        samyTotemTimersDb.version = 2.1
+    end
+
+    if (IsDoDbVersion(2.3, samyTotemTimersDb)) then
+        samyTotemTimersDb.totemLists = setDefault(SamyTotemTimersDB.totemLists, SamyTotemTimersConfig.defaultTotemLists, true)  
+        SamyTotemTimersUtils:Print("Updated twist totem list with more totems, added order to totemlists")
+        samyTotemTimersDb.version = 2.3
+    end
+
+    if (IsDoDbVersion(2.5, samyTotemTimersDb)) then
+        samyTotemTimersDb.totemLists[1]["totems"]["Tremor Totem"]["PulseTime"] = 4
+        samyTotemTimersDb.totemLists[5]["totems"]["Tremor Totem"]["PulseTime"] = 4
+        SamyTotemTimersUtils:Print("Updated pulse timers for tbc")
+        samyTotemTimersDb.version = 2.5
+    end
+end
